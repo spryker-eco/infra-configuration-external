5d44ddca1e2dc51294f6259ef4e022f999f75c7c
Ivan Dallaserra <ivan.dallaserra@spryker.com>
2022-12-28 14:17:45 +0100

    test pr customer 1 commit

--- customer1/project1/testenv1/main.tf
+++ customer1/project1/testenv1/main.tf
@@ -1,14 +1,14 @@
-resource "aws_s3_bucket" "testenv1-bucket" {
-  bucket = "testenv1-bucket"
+resource "aws_s3_bucket" "testenv3-bucket" {
+  bucket = "testenv3-bucket"
 
   tags = {
     Name        = "My bucket"
     Customer    = "customer1"
-    Environment = "testenv1"
+    Environment = "testenv3"
   }
 }
 
 resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.testenv1-bucket.id
+  bucket = aws_s3_bucket.testenv3-bucket.id
   acl    = "private"
 }
